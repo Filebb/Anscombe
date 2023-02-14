062198e089511b41b43a421dac00e9036643f844
Filebb <fil.cavallari@gmail.com>
2023-02-14 10:56:05 +0000

    pivot longer approach

--- make_tidy_csv.R
+++ make_tidy_csv.R
@@ -1,12 +1,20 @@
 library(tidyverse)
 
-group_1 <- anscombe %>% select(x = x1, y = y1) %>% mutate(group = "Group_1")
-group_2 <- anscombe %>% select(x = x2, y = y2) %>% mutate(group = "Group_2")
-group_3 <- anscombe %>% select(x = x3, y = y3) %>% mutate(group = "Group_3")
-group_4 <- anscombe %>% select(x = x4, y = y4) %>% mutate(group = "Group_4")
+group_1 <- anscombe |> select(x = x1, y = y1) |> mutate(group = "Group_1")
+group_2 <- anscombe |> select(x = x2, y = y2) |> mutate(group = "Group_2")
+group_3 <- anscombe |> select(x = x3, y = y3) |> mutate(group = "Group_3")
+group_4 <- anscombe |> select(x = x4, y = y4) |> mutate(group = "Group_4")
 
 data <- bind_rows(group_1, group_2, group_3, group_4) %>% as_tibble()
 
 rm(group_1, group_2, group_3, group_4)
 
 data %>% write_csv(file = "anscombe.csv")
+
+# more elegant approach using pivot_longer
+anscombe |>
+    pivot_longer(everything(),
+                 cols_vary = "slowest",
+                 names_to = c(".value", "set"),
+                 names_pattern = "(.)(.)") |>
+    view()
